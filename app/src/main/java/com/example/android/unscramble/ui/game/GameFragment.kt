8de22ac6c7c571a4b7fb137ae24ed5a2b9bd55a3
apsareena <shaikzulekha3132@gmail.com>
2022-06-08 23:01:32 +0530

    removed commented code

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- */
-
 package com.example.android.unscramble.ui.game
 
 import android.os.Bundle
@@ -27,9 +11,6 @@ import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-/**
- * Fragment where the game is played, contains the game logic.
- */
 class GameFragment : Fragment() {
 
     private val viewModel: GameViewModel by viewModels()
@@ -37,18 +18,11 @@ class GameFragment : Fragment() {
 
 
 
-    // Binding object instance with access to the views in the game_fragment.xml layout
     private lateinit var binding: GameFragmentBinding
-
-    // Create a ViewModel the first time the fragment is created.
-    // If the fragment is re-created, it receives the same GameViewModel instance created by the
-    // first fragment
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout XML file and return a binding object instance
         binding = GameFragmentBinding.inflate(inflater, container, false)
         Log.d("GameFragment", "GameFragment created/re-created!")
         Log.d("GameFragment", "Word: ${viewModel.currentScrambledWord} " +
@@ -63,11 +37,9 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
-        // Update the UI
+
 //        updateNextWordOnScreen()
 //        binding.score.text = getString(R.string.score, 0)
 //        binding.wordCount.text = getString(
@@ -86,10 +58,7 @@ class GameFragment : Fragment() {
             })
     }
 
-    /*
-    * Checks the user's word, and updates the score accordingly.
-    * Displays the next scrambled word.
-    */
+
     private fun onSubmitWord() {
 //        if (viewModel.isUserWordCorrect(playerWord)){
 //            setErrorTextField(false)
@@ -129,10 +98,7 @@ class GameFragment : Fragment() {
                 restartGame()
             }.show()
     }
-    /*
-     * Skips the current word without changing the score.
-     * Increases the word count.
-     */
+
     private fun onSkipWord() {
         if (viewModel.nextWord()){
             setErrorTextField(false)
@@ -142,35 +108,25 @@ class GameFragment : Fragment() {
         }
     }
 
-    /*
-     * Gets a random word for the list of words and shuffles the letters in it.
-     */
+
 //    private fun getNextScrambledWord(): String {
 //        val tempWord = allWordsList.random().toCharArray()
 //        tempWord.shuffle()
 //        return String(tempWord)
 //    }
 
-    /*
-     * Re-initializes the data in the ViewModel and updates the views with the new data, to
-     * restart the game.
-     */
+
     private fun restartGame() {
         viewModel.reinitializeData()
         setErrorTextField(false)
 //        updateNextWordOnScreen()
     }
 
-    /*
-     * Exits the game.
-     */
+
     private fun exitGame() {
         activity?.finish()
     }
 
-    /*
-    * Sets and resets the text field error status.
-    */
     private fun setErrorTextField(error: Boolean) {
         if (error) {
             binding.textField.isErrorEnabled = true
@@ -181,9 +137,7 @@ class GameFragment : Fragment() {
         }
     }
 
-    /*
-     * Displays the next scrambled word on screen.
-     */
+
 //    private fun updateNextWordOnScreen() {
 //        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
 //    }
